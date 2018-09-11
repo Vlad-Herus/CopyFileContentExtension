df6a602f92f2802e073a5884222a207e086d66da
Vladimir <vladimir.herus@centralinnovation.com>
2018-09-12 00:57:39 +1000

    Added : extension solution with initial command

---
+++ CopyFileContentExtension/CopyCommand.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CopyFileContentExtension.Services;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace CopyFileContentExtension
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CopyCommand
+    {
+        /// <summary>
+        /// Command will only be show for files with this extension
+        /// </summary>
+        const string SUPPORTED_EXT = ".sql";
+
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("b19477f2-7633-4310-b57c-8fe4ad522e69");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage m_Package;
+        private readonly SelectionService m_Selection;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CopyCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CopyCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.m_Package = package ?? throw new ArgumentNullException(nameof(package));
+            m_Selection = new SelectionService();
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            OleMenuCommand menuItem = new OleMenuCommand(this.Execute, menuCommandID);
+            menuItem.BeforeQueryStatus += Before_Execute;
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CopyCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.m_Package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CopyCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new CopyCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            MessageBox.Show("1234");
+
+        }
+
+        private async void Before_Execute(object sender, EventArgs e)
+        {
+            if (sender is OleMenuCommand menuCommand)
+            {
+                var selectedItems = await m_Selection.GetFullFilePathAsync(this.ServiceProvider);
+
+                if (selectedItems.All(path =>
+                    string.Equals(Path.GetExtension(path), SUPPORTED_EXT, StringComparison.CurrentCultureIgnoreCase)))
+                {
+                    menuCommand.Visible = true;
+                }
+                else
+                {
+                    menuCommand.Visible = false;
+                }
+            }
+        }
+    }
+}
---
+++ CopyFileContentExtension/Services/SelectionService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFileContentExtension.Services
+{
+    class SelectionService
+    {
+        public async Task<IEnumerable<string>> GetFullFilePathAsync(Microsoft.VisualStudio.Shell.IAsyncServiceProvider serviceProvider)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var selectionMonitor = await serviceProvider.GetServiceAsync(typeof(IVsMonitorSelection)) as IVsMonitorSelection;
+            if (selectionMonitor == null)
+            {
+                throw new Exception("Failed to get SVsShellMonitorSelection service.");
+            }
+
+            int hr = selectionMonitor.GetCurrentSelection(out IntPtr hierarchyPtr, out uint itemid, out IVsMultiItemSelect multiSelect, out IntPtr containerPtr);
+
+            if (hierarchyPtr == IntPtr.Zero)
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            if (IntPtr.Zero != containerPtr)
+            {
+                Marshal.Release(containerPtr);
+                containerPtr = IntPtr.Zero;
+            }
+            Debug.Assert(hr == VSConstants.S_OK, "GetCurrentSelection failed.");
+
+            if (itemid == (uint)VSConstants.VSITEMID.Selection)
+            {
+                multiSelect.GetSelectionInfo(out uint count, out int somethingElse);
+                if (count > 0)
+                {
+                    VSITEMSELECTION[] selectedItems = new VSITEMSELECTION[count];
+                    var sel = multiSelect.GetSelectedItems((uint)__VSGSIFLAGS.GSI_fOmitHierPtrs, count, selectedItems);
+
+                    //multiple selection
+                    return selectedItems.Select(info => GetItemPath(hierarchyPtr, info.itemid));
+                }
+                else
+                {
+                    return Enumerable.Empty<string>();
+                }
+            }
+            else
+            {
+                return new string[] { GetItemPath(hierarchyPtr, itemid) };
+            }
+        }
+
+        string GetItemPath(IntPtr hirarchryPtr, uint temId)
+        {
+#pragma warning disable VSTHRD010 // Invoke single-threaded types on Main thread
+            IVsProject project = Marshal.GetUniqueObjectForIUnknown(hirarchryPtr) as IVsProject;
+            string itemFullPath = null;
+            project?.GetMkDocument(temId, out itemFullPath);
+#pragma warning restore VSTHRD010 // Invoke single-threaded types on Main thread
+
+            return itemFullPath;
+        }
+    }
+}
